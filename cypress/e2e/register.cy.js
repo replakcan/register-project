3ac1a3cc9f78770b807b4c93a6637a45a4c03309
replakcan <alper.makcan@gmail.com>
2024-09-06 14:54:41 +0300

    Cypress test for "ad" and "soyad" inputs

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,3 +1,5 @@
+import { errorMessages } from "../../src/components/Register";
+
 describe('Register Page', () => {
   describe("Error Messages", () => {
     it("name input throws error for 2 chars", () => {
@@ -5,9 +7,19 @@ describe('Register Page', () => {
       //arrange
       cy.visit("http://localhost:5173/");
       //act
-      cy.get('[data-cy="ad-input"]').type("em")
+      cy.get('[data-cy="ad-input"]').type("al")
       //assert
       cy.contains(errorMessages.ad)
+    })
+
+    it("name input throws error for 2 chars", () => {
+
+      //arrange
+      cy.visit("http://localhost:5173/");
+      //act
+      cy.get('[data-cy="soyad-input"]').type("ak")
+      //assert
+      cy.contains(errorMessages.soyad)
     })
   })
 })
