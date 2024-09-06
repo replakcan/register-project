f55cbb5f2e83924fabc0eee23cd23b110176d7e8
replakcan <alper.makcan@gmail.com>
2024-09-06 14:58:07 +0300

    Add cypress test for password and email inputs

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -2,7 +2,7 @@ import { errorMessages } from "../../src/components/Register";
 
 describe('Register Page', () => {
   describe("Error Messages", () => {
-    it("name input throws error for 2 chars", () => {
+    it("Name input throws error for 2 chars", () => {
 
       //arrange
       cy.visit("http://localhost:5173/");
@@ -12,7 +12,7 @@ describe('Register Page', () => {
       cy.contains(errorMessages.ad)
     })
 
-    it("name input throws error for 2 chars", () => {
+    it("Surname input throws error for 2 chars", () => {
 
       //arrange
       cy.visit("http://localhost:5173/");
@@ -20,6 +20,26 @@ describe('Register Page', () => {
       cy.get('[data-cy="soyad-input"]').type("ak")
       //assert
       cy.contains(errorMessages.soyad)
+    })
+    
+    it("Email input throws error for alper@wit.", () => {
+
+      //arrange
+      cy.visit("http://localhost:5173/");
+      //act
+      cy.get('[data-cy="email-input"]').type("alper@wit.")
+      //assert
+      cy.contains(errorMessages.email)
+    })
+
+    it("Password input throws error for 1234", () => {
+
+      //arrange
+      cy.visit("http://localhost:5173/");
+      //act
+      cy.get('[data-cy="password-input"]').type("1234")
+      //assert
+      cy.contains(errorMessages.password)
     })
   })
 })
